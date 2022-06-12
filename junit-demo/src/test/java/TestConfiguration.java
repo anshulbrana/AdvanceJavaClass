6d0bf197aab8584556900b96e1859b025aa5aaaf
anshulbrana1 <anshul@Anshuls-MacBook-Air.local>
2022-06-12 20:29:47 +0200

    Class 4: Multi Threading example in java

--- junit-demo/src/test/java/TestConfiguration.java
+++ junit-demo/src/test/java/TestConfiguration.java
@@ -4,10 +4,11 @@ import services.Configuration;
 
 public class TestConfiguration {
 
-//    //static because It will be executed before everything
-//    static {
-//        System.setProperty("conf.file","src/test/resources/conf.properties");
-//    }
+ /**   //static because It will be executed before everything
+    static {
+        System.setProperty("conf.file","src/test/resources/conf.properties");
+    }
+  **/
 
     @Test
     public void fetchTestValue(){
